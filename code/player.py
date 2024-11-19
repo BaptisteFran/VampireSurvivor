656b3fcce3f2cf55534b6e1c006388b8efde9587
Baptiste <baptiste.franel@gmail.com>
2024-11-19 10:00:18 +0100

    added bullets

--- code/player.py
+++ code/player.py
@@ -1,8 +1,11 @@
 from settings import *
+from sprites import Bullet
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_images()
+        self.state, self.frame_index = 'down', 0
         self.image = pygame.image.load(join("images", "player", "down", "0.png")).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
         self.hitbox_rect = self.rect.inflate(-60, -90)
@@ -12,11 +15,22 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.Vector2()
         self.collision_sprites = collision_sprites
 
+    def load_images(self):
+        self.frames = {'left': [], 'right': [], 'up': [], 'down': []} # has to match the folders names
+
+        for state in self.frames.keys():
+            for folder_path, sub_folders, file_names in walk(join('images', 'player', state)): # walk from os is used to walk through a folder
+                if file_names:
+                    for file_name in sorted(file_names, key= lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
+
     def input(self):
         keys = pygame.key.get_pressed()
         # int() is not necessary, but for comprehension, i will put it
-        self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_q])
-        self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_z])
+        self.direction.x = int(keys[pygame.K_d] or keys[pygame.K_RIGHT]) - int(keys[pygame.K_q] or keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_s] or keys[pygame.K_DOWN]) - int(keys[pygame.K_z] or keys[pygame.K_UP])
         # normalize
         self.direction = self.direction.normalize() if self.direction != pygame.Vector2() else self.direction
 
@@ -37,7 +51,19 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
                     if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
 
+    def animate(self, delta_time):
+        # get state
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+
+        # animate
+        self.frame_index = self.frame_index + 5 * delta_time if self.direction else 0
+
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
     def update(self, delta_time):
         self.input()
         self.move(delta_time)
-
+        self.animate(delta_time)
